ab4feb9b41def9092b83f73246982619e35b51f0
Muhammad <muhammadkadirov089@gmail.com>
2024-03-22 13:59:06 +0100

    added Converter Page

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import styles from './Converter.module.css'
 
 function Converter() {
   return (
-    <div>Converter</div>
+    <div className={styles.container}>
+
+      <div className={styles.converterContainer}>
+          <h3 className={styles.converterHeadingText}> Crypto Converter </h3>
+          <div className={styles.convertInput}>
+
+          </div>
+          
+          <div className={styles.converterOptionsContainer}>
+              <div className={styles.convertFirstOption}>
+                  <select id="firstOption">
+                    <option value=""> Convert from ... </option>
+                  </select>
+              </div>
+              <div className={styles.convertSecondOption}>
+                  <select id="secondOption">
+                    <option value=""> to ... </option>
+                    {/* {curency.map(el)=>} */}
+                  </select>
+              </div>
+              <div className={styles.converterButtonBlock}>
+                <button className={styles.converterButton}>
+                  Convert
+                </button>
+              </div>
+          </div>     
+    
+      </div>
+
+          <div className={converterResult}>
+
+          </div>
+
+    </div>
   )
 }
 
